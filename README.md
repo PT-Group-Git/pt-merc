a11190438513600d530df2ee16e856555cc76bb8
Jared <jaredksemail@gmail.com>
2022-06-18 20:57:07 -0400

    Updating readme and version 1.0 of script

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# pt-merc
+# Pt-Merc Userscript
+For help with userscript contact [ThtAstronautGuy](https://www.torn.com/profiles.php?XID=1977683) on Torn.
+# Terms of use
+This script is provided without warranty or guarentee, etc etc.
+Your API key is only stored locally, and WILL NOT be sent to us. By using this script you agree that your battle stats will be sent to our merc team so that they will know if they can hit you or not. Hosps are done on best effort only, and if there is no one on to hit you, you will not be hit.
+# How to use
+Hitting the update key button will allow you to set your key. This will validate that your key is valid, and should clear the saved key if you enter an incorrect one. 
+If you hit "Hosp Me" and your key is not set, it will prompt you to add your key, then you will most likely have to hit the "Hosp Me" button again.
+Premium hosps can only be done with advance notice, as the weapon must be loaned out to the member from the PT-Family armoury, as such that button will link to our thread, where you can see the online status of all members of the merc team. 
--- userscript.js
+++ userscript.js
@@ -1,169 +1,146 @@
 // ==UserScript==
-// @name         PT Group Merc Hit Request
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @author       ThtAstronautGuy [1977683]
-// @description  Send merc request to PT Group
-// @match        https://www.torn.com/*
-// @grant        GM_xmlhttpRequest
-// @grant        GM_addStyle
-// @connect      thatastronautguy.space
+// @name          PT Group Merc Hit Request
+// @namespace     http://tampermonkey.net/
+// @version       0.1
+// @author        ThtAstronautGuy [1977683]
+// @description   Send merc request to PT Group
+// @match         https://www.torn.com/*
+// @grant         GM_xmlhttpRequest
+// @grant         GM_addStyle
+// @connect       thatastronautguy.space
+// @connect       api.torn.com
+// @updateURL     https://raw.githubusercontent.com/ThatAstronautGuy/pt-merc/main/userscript.js
+// @downloadURL   https://raw.githubusercontent.com/ThatAstronautGuy/pt-merc/main/userscript.js
 // ==/UserScript==
- 
-/*
-CHANGE LOG:
-Version 0.1 First Version
-*/
+
+let apiKey = window.localStorage.getItem('torn-pt-api-key') || null;
 
 (function() {
-    'use strict';
-    var apiKey = `rCRrF2GjYLpCS7HY`
-    var hospButton = `
+  var hospButton = `
 <div id="top-page-links-list" class="content-title-links" role="list" aria-labelledby="top-page-links-button">
-<a role="button" aria-labelledby="pt-hosp" class="pt-hosp t-clear h c-pointer  m-icon line-h24 right last" href="#" style="padding-left: 10px; padding-right: 10px" id="pt-hosp-link">
+<a role="button" aria-labelledby="pt-hosp" class="pt-hosp t-clear h c-pointer  m-icon line-h24 right last" href="#" style="padding-left: 10px; padding-right: 10px" id="pt-hosp-api">
+    <span id="pt-hosp" style="color:rgb(255,233,0)">Update Key</span>
+</a>
+<a role="button" aria-labelledby="pt-hosp" class="pt-hosp t-clear h c-pointer  m-icon line-h24 right last" href="#" style="padding-left: 10px; margin-right: 0px" id="pt-hosp-link">
     <span id="pt-hosp" style="color:rgb(218,41,28)">Hosp Me</span>
 </a>
-<a role="button" aria-labelledby="pt-hosp" class="pt-hosp t-clear h c-pointer  m-icon line-h24 right last" href="#" style="padding-left: 10px; margin-right: 0px;" id="pt-hosp-premium-link">
-    <span id="pt-hosp" style="color:rgb(4,106,56); padding-left: 3px;">Premium</span>
+<a role="button" aria-labelledby="pt-hosp" class="pt-hosp t-clear h c-pointer  m-icon line-h24 right last" href="https://www.torn.com/forums.php#/p=threads&f=10&t=16262963&b=0&a=0" style="padding-left: 10px; margin-right: 0px; color:rgb(4,106,56);" id="pt-hosp-premium-link">
+    Premium
 </a>
 </div>
-    `
- 
- 
+  `
+
+  setTimeout(addLink, 500);
+
+  function addLink(){
+    if(!document.getElementById('pt-hosp-link')){
+      let linkReference = document.querySelector('.links-footer') || document.querySelector('.content-title .clear') || document.querySelector('.tutorial-switcher') || document.querySelector('.links-top-wrap') || document.querySelector('.forums-main-wrap');
+      if(linkReference){
+          let linkContainer = linkReference.parentNode;
+          let newElelemt = document.createElement('span');
+          newElelemt.innerHTML = hospButton;
+          linkContainer.insertBefore(newElelemt, linkReference);
+
+          document.getElementById('pt-hosp-link').addEventListener('click', callForHosp);
+          document.getElementById('pt-hosp-api').addEventListener('click', updateAPIKeyEvent);
+      }
+    }
+
     setTimeout(addLink, 500);
- 
-    function addLink(){
- 
-        if(!document.getElementById('pt-hosp-link')){
-            let linkReference = document.querySelector('.links-footer') || document.querySelector('.content-title .clear') || document.querySelector('.tutorial-switcher') || document.querySelector('.links-top-wrap') || document.querySelector('.forums-main-wrap');
-            if(linkReference){
-                let linkContainer = linkReference.parentNode;
-                let newElelemt = document.createElement('span');
-                newElelemt.innerHTML = hospButton;
-                linkContainer.insertBefore(newElelemt, linkReference);
- 
-                document.getElementById('pt-hosp-link').addEventListener('click', callForRegularHosp);
-                document.getElementById('pt-hosp-premium-link').addEventListener('click', callForPremiumHosp);
+  }
+
+  function updateAPIKeyEvent(e){
+    e.preventDefault;
+    updateAPIKey("Please enter your API key to save it");
+  }
+
+  function updateAPIKey(message){
+    let key = prompt(message);
+    if(key == null || key == ""){
+      alert("Key not entered.");
+    }
+    else{
+      GM_xmlhttpRequest ({
+      method: 'POST',
+      url: 'https://api.torn.com/user/?selections=battlestats&key=' + key,
+      onload: (r) => {
+        if(r.status == 200){
+          const obj = JSON.parse(r.responseText);
+          if(obj.hasOwnProperty('error')){
+            var errorCode = obj.error.code;
+            if(errorCode == 2){
+              alert("Incorrect key. Enter a proper key.");
             }
-        }
- 
-        setTimeout(addLink, 500);
-    }
- 
-    function callForRegularHosp(e){
-        e.preventDefault;
-        callForHosp(false)
-    }
- 
-    function callForPremiumHosp(e){
-        e.preventDefault;
-        callForHosp(true)
-    }
- 
-    function callForHosp(premium){
-        let PlayerID = null;
-        let PlayerName = null;
-        let factionIcon = null;
-        let faction = "";
-        let battleStats = "";
- 
-        let country = document.body.dataset.country;
- 
-        let Country = [{
-            name: 'mexico',
-            title: 'Mexico'
-        }, {
-            name: 'cayman-islands',
-            title: 'Cayman Islands'
-        }, {
-            name: 'canada',
-            title: 'Canada'
-        }, {
-            name: 'hawaii',
-            title: 'Hawaii'
-        }, {
-            name: 'uk',
-            title: 'United Kingdom'
-        }, {
-            name: 'argentina',
-            title: 'Argentina'
-        }, {
-            name: 'switzerland',
-            title: 'Switzerland'
-        }, {
-            name: 'japan',
-            title: 'Japan'
-        }, {
-            name: 'china',
-            title: 'China'
-        }, {
-            name: 'uae',
-            title: 'UAE'
-        }, {
-            name: 'south-africa',
-            title: 'South Africa'
-        }, {
-            name: 'torn',
-            title: 'Torn'
-        }].find((destination) => country == destination.name);
- 
-        if(!Country){
-            Country = {
-                name: 'torn',
-                title: 'Torn'
+            else if(errorCode == 5 || errorCode == 8){
+              alert("Too many requests. Please try again soon.");
             }
-        }
- 
-        try{
-            let uid = getCookie('uid');
-            let data = JSON.parse(sessionStorage.getItem('sidebarData' + uid));
-            if(data && data.user && data.statusIcons && data.statusIcons.icons){
-                PlayerID = uid;
-                PlayerName = `${data.user.name} [${uid}]`;
-                factionIcon = data.statusIcons.icons.faction;
+            else if(errorCode == 9){
+              alert("API system disabled. Please try again soon.");
+            }
+            else if(errorCode == 14){
+              alert("Daily read limited reached. Please wait until the next day or reach out through our thread.");
+            }
+            else if(errorCode == 16){
+              alert("Key access level is not high enough. A limited access API key is required.");
             }
             else{
-                alert('Error getting player information')
-                return;
+              alert("Unknown error with key. Please try another key or try again later.");
             }
+            window.localStorage.setItem('torn-pt-api-key', "");
+          }
+          else{
+            alert("Key saved.");
+            window.localStorage.setItem('torn-pt-api-key', key);
+            apiKey = key;
+          }
         }
-        catch(error){
-            alert('Error getting player information')
-            return;
+        else{
+          alert("HTTP Error Code: " + r.status);
+          window.localStorage.setItem('torn-pt-api-key', "");
         }
+      },
+      onerror: () =>{
+        alert("Couldn't check. Error");
+      },
+      onabort: () =>{
+        alert("Couldn't check. Aborted");
+      },
+      onprogress: () =>{
+      },
+      ontimeout: () =>{
+        alert("Couldn't check. Timeout");
+      }
+    })
+    }
+  }
 
-        if(factionIcon){
-            faction = factionIcon.subtitle;
-        }
- 
-        if(PlayerName){
-            var postData = {uid: PlayerID, Player: PlayerName, Faction: faction, Country: Country.title, Premium: premium};
- 
+  function callForHosp(e){
+    e.preventDefault;
+    let confirmAction = confirm("Are you sure you want to request a hosp at a cost of 4m?");
+    if(apiKey == null || apiKey == ""){
+      updateAPIKey("Please enter your API key then try again");
+    }
+    if(confirmAction) {
+      GM_xmlhttpRequest ({
+        method: 'POST',
+        url: 'https://api.torn.com/user/?selections=profile,battlestats&key=' + apiKey,
+        onload: (r) => {
+          const obj = JSON.parse(r.responseText);
+            var postData = {PlayerID: obj.player_id, PlayerName: obj.name, Strength: obj.strength, Dexterity: obj.dexterity, Defense: obj.defense, Speed: obj.speed, FactionID: obj.faction.faction_id, FactionName: obj.faction.faction_name, Revivable: obj.revivable, Status: obj.status.description};
+            alert(JSON.stringify(postData));
             GM_xmlhttpRequest ( {
-                method:     'POST',
-                url:        'https://www.thatastronautguy.space/dev/hospme.php',
-                data:       JSON.stringify(postData),
-                onload:     function (responseDetails) {
-                    // DO ALL RESPONSE PROCESSING HERE...
-                    //console.log(responseDetails, responseDetails.responseText);
-                    alert(responseDetails.responseText);
-                }
-            });
+              method:     'POST',
+              url:        'https://www.thatastronautguy.space/api/gethosp',
+              data:       JSON.stringify(postData),
+              onload:     function (responseDetails) {
+                  alert(responseDetails.responseText);
+              }
+          });
         }
+      });
     }
- 
-    function getCookie(cname) {
-        var name = cname + "=";
-        var ca = document.cookie.split(';');
-        for(var i = 0; i < ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) == ' ') {
-                c = c.substring(1);
-            }
-            if (c.indexOf(name) == 0) {
-                return c.substring(name.length, c.length);
-            }
-        }
-        return "";
+    else {
+      alert("Request not sent.");
     }
+  }
 })();
